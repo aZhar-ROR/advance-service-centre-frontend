5ed379c0ac22792663fbaea6a6e6f3b6cab0fd8f
Azhar <azhar.me.1708@gmail.com>
2025-07-05 17:42:20 +0530

    fix: Correct endpoint URL in submitContactForm utility to include API path

--- src/utils/submitContactForm.js
+++ src/utils/submitContactForm.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export async function submitContactForm(
   formData,
-  endpoint = `${import.meta.env.VITE_API_BASE_URL || ""}/send-message`
+  endpoint = `${import.meta.env.VITE_API_BASE_URL || " "}/api/send-message`
 ) {
   try {
     await axios.post(endpoint, formData);
